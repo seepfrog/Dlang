3331e3b7493f385000360c509a02661cfead0769
seepfrog <cyadd20@gmail.com>
2015-10-26 03:24:55 +0900

    switch fizzbuzz

--- fizzbuzz.d
+++ fizzbuzz.d
@@ -2,11 +2,11 @@ import std.stdio;
 
 int main(char[][] arg)
 {
-    fizzbuzz_generator_by_if(100);
+    fizzbuzz_generator_by_switch(100);
     return 0;
 }
 
-/*fizzbuzz!*/
+/*-------if fizzbuzz!----------------*/
 void fizzbuzz_generator_by_if(int num){
   foreach(x;1 .. num){
     if(x%15==0){
@@ -21,11 +21,21 @@ void fizzbuzz_generator_by_if(int num){
   }
 }
 
+/*-----------write fizzbuzz!-------------*/
+
 auto fizz(){writeln("fizz");}
 void buzz(){ writeln("buzz");}
 void fizzbuzz(){ writeln("fizzbuzz");}
 
+/*--------------switch fizzbuzz!---------*/
 
-void fizzbuzz_by_switch(int num){
-
+void fizzbuzz_generator_by_switch(int num){
+    foreach(x;1 .. num){
+      switch (x%15) {
+        case 0          : fizzbuzz();break;
+        case 3,6,9,12   : fizz();break;
+        case 5,10       : buzz();break;
+        default         : writeln(x);break;
+      }
+    }
 }
