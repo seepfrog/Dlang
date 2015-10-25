ac2cad064166cf6963f13e5838078bd5ff8de7a9
seepfrog <cyadd20@gmail.com>
2015-10-26 03:37:21 +0900

    good

--- fizzbuzz.d
+++ fizzbuzz.d
@@ -10,32 +10,37 @@ int main(char[][] arg)
 void fizzbuzz_generator_by_if(int num){
   foreach(x;1 .. num){
     if(x%15==0){
-      fizzbuzz();
+      write_fizzbuzz("fizzbuzz");
     }else if(x%3==0){
-      fizz();
+      write_fizzbuzz("fizz");
     }else if(x%5==0){
-      buzz();
+      write_fizzbuzz("buzz");
     }else {
       writeln(x);
     }
   }
 }
 
-/*-----------write fizzbuzz!-------------*/
-
-auto fizz(){writeln("fizz");}
-void buzz(){ writeln("buzz");}
-void fizzbuzz(){ writeln("fizzbuzz");}
-
 /*---------switch fizzbuzz!---------------*/
 
 void fizzbuzz_generator_by_switch(int num){
     foreach(x;1 .. num){
       switch (x%15) {
-        case 0          : fizzbuzz();break;
-        case 3,6,9,12   : fizz();break;
-        case 5,10       : buzz();break;
+        case 0          : write_fizzbuzz("fizzbuzz");break;
+        case 3,6,9,12   : write_fizzbuzz("fizz");break;
+        case 5,10       : write_fizzbuzz("buzz");break;
         default         : writeln(x);break;
       }
+    }
+}
+
+/*---------write fizzbuzz---------------*/
+
+auto write_fizzbuzz(string name){
+    switch(name){
+      case "fizz"     : writeln("fizz");break;
+      case "buzz"     : writeln("buzz");break;
+      case "fizzbuzz" : writeln("fizzbuzz");break;
+      default         : writeln("YOU MUST LOVE FIZZBUZZ!");break;
     }
 }
