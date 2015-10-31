9d86d5a498066c562d636b98b4477f4a0cc50680
seepfrog <cyadd20@gmail.com>
2015-11-01 02:19:26 +0900

    learning ufcs

---
+++ ufcs_test.d
@@ -0,0 +1,20 @@
+import std.stdio;
+
+void main()
+{
+    int a = 5;
+    2.f1.f2.f3.f1.writeln;
+    a.f1.f2.f3.writeln;
+}
+
+auto f1(int a){
+    return a * 3;
+}
+
+auto f2(int b){
+    return b * 7;
+}
+
+auto f3(int c){
+    return c * 11;
+}
