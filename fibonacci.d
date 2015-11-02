0bd430bc8c90725de7a01c335005942b31576124
seepfrog <cyadd20@gmail.com>
2015-11-03 00:15:42 +0900

    sawed‐off

--- fibonacci.d
+++ fibonacci.d
@@ -1,14 +1,12 @@
 import std.stdio,std.algorithm,std.range;
 
-int main(char[][] arg)
+void main()
 {
     foreach(i;0 .. 20){
-        writeln(fibonacci(i));
+        fibonacci(i).writeln;
     }
-    return 0;
 }
 
 int fibonacci(int n){
-    if(n < 2)return n;
-    return fibonacci(n-2) + fibonacci(n-1);
+    return n < 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
 }
